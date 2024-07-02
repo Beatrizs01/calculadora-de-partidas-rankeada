054d2f56e77d6db33087de5837b60343f0780c5a
Beatriz Santos <beatriz.s.s0104@gmail.com>
2024-07-02 01:49:48 -0300

    Projeto

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const vitorias = parseInt(prompt("Digite o número de vitórias:"));
+const derrotas = parseInt(prompt("Digite o número de derrotas:"));
+
+function calcularNivel(vitorias, derrotas){
+    const saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if(saldoVitorias < 10 ){
+        nivel = "Ferro";
+    }else if(saldoVitorias >= 11 &&  saldoVitorias <= 20){
+        nivel = "Bronze";
+    }else if(saldoVitorias >= 21 && saldoVitorias <= 50){
+        nivel = "Prata";
+    }else if(saldoVitorias >= 51 && saldoVitorias <= 80){
+        nivel = "Ouro";
+    }else if(saldoVitorias >= 81 && saldoVitorias <= 90){
+        nivel = "Diamante";
+    }else if(saldoVitorias >= 91 && saldoVitorias <= 100){
+        nivel = "Lendario";
+    }else {
+        nivel = "Imortal"
+    }
+
+    return `O Herói tem um saldo de ${saldoVitorias} vitórias e esta no nível ${nivel}`;
+}
+calcularNivel();
+const resultado = calcularNivel(vitorias, derrotas);
+console.log(resultado);
